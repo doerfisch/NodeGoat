9038fd6db904165f0510c1f3f68c9f6c42713364
Unknown <sebbl.schuler@gmail.com>
2017-11-06 20:47:18 +0100

    work on A7
    ...

--- artifacts/db-reset.js
+++ artifacts/db-reset.js
@@ -15,7 +15,7 @@ var USERS_TO_INSERT = [
         "userName": "admin",
         "firstName": "Node Goat",
         "lastName": "Admin",
-        "password": "Admin_321",
+        "password": "Admin_123",
         //"password" : "$2a$10$8Zo/1e8KM8QzqOKqbDlYlONBOzukWXrM.IiyzqHRYDXqwB3gzDsba", // Admin_123
         "isAdmin": true
     }, {
