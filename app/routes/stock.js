2640e54d39e4579a0963e81e11991fd6eafae73c
Unknown <sebbl.schuler@gmail.com>
2017-11-07 13:17:23 +0100

    Stock_LastPush

--- app/routes/stock.js
+++ app/routes/stock.js
@@ -21,7 +21,7 @@ function StockHandler(db) {
     this.updateStock = function(req, res, next) {
     	var itemId = req.body.itemId;
     	//var description = req.body.description;
-    	var price = parseInt(req.body.price);
+    	var price = parseInt(req.body.itemPrice);
 
     	stockDAO.updateStock(itemId, price, function(error) {
     	    if (error) return next(error);
